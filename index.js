1d48c2462e9d22084d3e446711b6b5d14d3e5105
SOS1617_jgg <josegallardog95@gmail.com>
2017-03-22 08:28:57 +0000

    Antony API uploaded on heroku

--- index.js
+++ index.js
@@ -478,3 +478,223 @@ app.delete(BASE_API_PATH + "/gdp-population-stats/:country", function (request, 
 
 
 ////////////////////////////////////////////////CODIGO API ANTONY////////////////////////////////////////////////////////////
+
+//GET every row of data
+app.get(BASE_API_PATH + "/rpc-stats", function (request, response) {
+    console.log("INFO: New GET/ received");
+    db2.find({}).toArray(function (err, data) {
+        if (err) {
+            console.error('WARNING: Error getting data from DB');
+            response.sendStatus(500); // internal server error
+        } else {
+            console.log("INFO: Sending out every row of data");
+            response.send(data);
+        }
+    });
+});
+
+
+//GET a single row
+app.get(BASE_API_PATH + "/rpc-stats/:country", function (request, response) {
+    var country = request.params.country;
+    
+    if (!country) {
+        console.log("WARNING: New GET request to /rpc-stats/:country without country, sending 400...");
+        response.sendStatus(400); // bad request
+    }else {
+        if(country == "loadInitialData"){
+            var alemania = new Object();
+            alemania.country = "Alemania";
+            alemania.year = 2017;
+            alemania.rpcyear = "54980";
+            alemania.rpcvariation = "1.6%";
+    
+            var francia = new Object;
+            francia.country = "Francia";
+            francia.year = 2014;
+            francia.rpcyear = "56.238";
+            francia.rpcvariation = "1.3%";
+    
+            console.log("INFO: Initializing data.");
+    
+            db2.find({}, function(err, countries){
+                if(err){
+                    response.sendStatus(500); // internal server error
+                }else{
+                    if(countries.length > 0){
+                        response.sendStatus(501);//Not implemented
+                    }else{
+                        db2.insert(alemania);
+                     db2.insert(francia);
+                     response.sendStatus(201); //created!
+                     console.log("INFO: Data initialized.");
+                    }
+                }
+            });
+        } else{
+            console.log("INFO: New GET request to /rpc-stats/" + country);
+        
+            db2.findOne({ country: country }, function(err,data){
+                    if(err){
+                        response.sendStatus(404);
+                    }else{
+                        response.send(data);
+                    }
+                });
+        }
+        
+    }
+});
+
+//POST over a collection
+app.post(BASE_API_PATH + "/rpc-stats", function (request, response) {
+    var newCountry = request.body;
+    if (!newCountry) {
+        console.log("WARNING: New POST request to /gdp-population-stats/ without Country to create, sending 400...");
+        response.sendStatus(400); // bad request
+    } else {
+        console.log("INFO: New POST request to /gdp-population-stats");
+        if (!newCountry.country || !newCountry.year /*|| !newCountry.rpc-year || !newCountry.rpc-variation*/) {
+            console.log("WARNING: The country is not well-formed, sending 422...");
+            response.sendStatus(422); // unprocessable entity
+        } else {
+            db2.find({country: newCountry.country}, function (err, countriesBeforeInsertion) {
+                if (err) {
+                    console.error('WARNING: Error getting data from DB');
+                    response.sendStatus(500); // internal server error
+                } else {
+                    
+                    if (countriesBeforeInsertion.length > 0) {
+                        console.log("WARNING: The contact " + JSON.stringify(newCountry, 2, null) + " already extis, sending 409...");
+                        response.sendStatus(409); // conflict
+                    } else {
+                        console.log("INFO: Adding contact " + JSON.stringify(newCountry, 2, null));
+                        db2.insert(newCountry);
+                        response.sendStatus(201); // created
+                    }
+                }
+            });
+        }
+    }
+});
+
+//post to one country--> method not allowed!!
+app.post(BASE_API_PATH + "/rpc-stats/:country", function (request, response){
+    response.sendStatus(405);//method not allowed
+});
+
+//PUT to the entire api (update all the elements)
+app.put(BASE_API_PATH + "/rpc-stats", function (request, response){
+   response.sendStatus(405);//Method not allowed!! 
+});
+
+//update a single element
+app.put(BASE_API_PATH + "/:country", function (request, response){
+    var updatedCountry = request.body;
+    var country = request.params.country;
+    
+    if (!updatedCountry) {
+        response.sendStatus(400); // bad request
+    }else{
+        console.log("INFO: New POST request to /gdp-population-stats");
+        if (!updatedCountry.country || !updatedCountry.year /*|| !updatedCountry.gdp-year || !updatedCountry.population-year*/) {
+            console.log("WARNING: The country is not well-formed, sending 422...");
+            response.sendStatus(422); // unprocessable entity
+        }else{
+            db2.find({country: country}, function (err, countries) {
+                if (err) {
+                    console.error('WARNING: Error getting data from DB');
+                    response.sendStatus(500); // internal server error
+                } else {
+                    
+                    if(countries.length === 0){
+                        console.log("WARNING: There is not any country with name " + country);
+                        response.sendStatus(404); // not found
+                    }else{
+                        db2.update({country: country}, updatedCountry);
+                        response.send(updatedCountry); // return the updated contact
+                    }
+                    
+                }
+            });
+        }
+    }
+});
+
+//DELETE over all the rows
+app.delete(BASE_API_PATH + "/rpc-stats", function (request, response) {
+    console.log("INFO: New FULL DELETE request");
+    db2.remove({}, {multi: true}, function (err, numRemoved) {
+        if (err) {
+            console.error('WARNING: Error removing data from DB');
+            response.sendStatus(500); // internal server error
+        } else {
+            if (numRemoved > 0) {
+                console.log("INFO: All the countries (" + numRemoved + ") have been succesfully deleted, sending 204...");
+                response.sendStatus(204); // no content
+            } else {
+                console.log("WARNING: There are no countries to delete");
+                response.sendStatus(404); // not found
+            }
+        }
+    });
+});
+
+app.delete(BASE_API_PATH + "/rpc-stats/:country", function (request, response) {
+    var country = request.params.country;
+    if (!country) {
+        console.log("WARNING: New DELETE request to /gdp-population-stats/:country without name, sending 400...");
+        response.sendStatus(400); // bad request
+    } else {
+        console.log("INFO: New DELETE request to /gdp-population-stats/" + country);
+        db2.remove({country: country}, {}, function (err, numRemoved) {
+            if (err) {
+                console.error('WARNING: Error removing data from DB');
+                response.sendStatus(500); // internal server error
+            } else {
+                console.log("INFO: Country successfully removed.");
+                if (numRemoved === 1) {
+                    console.log("INFO: The country with name " + country + " has been succesfully deleted, sending 204...");
+                    response.sendStatus(204); // no content
+                } else {
+                    console.log("WARNING: There are no countries to delete");
+                    response.sendStatus(404); // not found
+                }
+            }
+        });
+    }
+});
+
+app.get(BASE_API_PATH + "/rpc-stats/loadInitialData", function (request, response){
+    
+    var alemania = new Object();
+    alemania.country = "Alemania";
+    alemania.year = 2017;
+    alemania.rpcyear = "56.238";
+    alemania.rpcvariation = "1.6%";
+    
+    var francia = new Object;
+    francia.country = "Francia";
+    francia.year = 2014;
+    francia.rpcyear = "50.887.30";
+    francia.rpcvariation = "1.2%";
+    
+    console.log("INFO: Initializing data.");
+    
+    db2.find({}, function(err, countries){
+        if(err){
+            response.sendStatus(500); // internal server error
+        }else{
+            if(countries.length > 0){
+                response.sendStatus(501);//Not implemented
+            }else{
+                db2.insert(alemania);
+                db2.insert(francia);
+                response.sendStatus(201); //created!
+                console.log("INFO: Data initialized.");
+            }
+        }
+    });
+    
+    
+});
