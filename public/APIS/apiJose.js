5c234c215ce44a3c12d43c6842fce41a2c3c2225
SOS1617_jgg <josegallardog95@gmail.com>
2017-03-28 16:54:44 +0000

    Upload to Heroku

--- public/APIS/apiJose.js
+++ public/APIS/apiJose.js
@@ -200,7 +200,7 @@ app.post(BASE_API_PATH + "/smi-stats", function (request, response) {
         //Si le falta algun parámetro al nuevo elemento que queremos introducir con el POST, devolvemos error
         if (!newCountry.country || !newCountry.year || !newCountry["smi-year"]|| !newCountry["smi-year-variation"]) {
             console.log("WARNING: The contact " + JSON.stringify(newCountry, 2, null) + " is not well-formed, sending 422...");
-            response.sendStatus(422); // unprocessable entity
+            response.sendStatus(400); // bad request
             
         } else {
             dbJose.find({}, function (err, smi_stats) {
@@ -265,7 +265,7 @@ app.put(BASE_API_PATH + "/smi-stats/:country", function (request, response) {
         //Si los datos recogidos en el comando CURL no contienen algunos de estos atributos, habrá error.
         if (!updatedCountry.country || !updatedCountry.year || !updatedCountry["smi-year"]|| !updatedCountry["smi-year-variation"]) {
             console.log("WARNING: The country " + JSON.stringify(updatedCountry, 2, null) + " is not well-formed, sending 422...");
-            response.sendStatus(422); // unprocessable entity
+            response.sendStatus(400); // Bad Request
         } else {
             //Buscamos los países que tengan el mismo nombre que el que se introduce en la URL
             //Los guardamos en un array
