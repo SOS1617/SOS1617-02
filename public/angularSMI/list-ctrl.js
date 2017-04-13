0b91c05900f3571132c54d7c692c328c50d13dd7
SOS1617_jgg <josegallardog95@gmail.com>
2017-04-13 18:28:32 +0000

    Last revision on code

--- public/angularSMI/list-ctrl.js
+++ public/angularSMI/list-ctrl.js
@@ -16,8 +16,17 @@ angular
             })
         }
         
-   // function refresh(){
-        //GET A COLECCIÓN CON PAGINACIÓN Y BÚSQUEDA
+    function refresh(){
+            $http
+                .get($scope.url+"?apikey="+ $scope.apikey +"&limit="+ $scope.limit +"&offset="+$scope.offset)
+                .then(function(response){
+                    $scope.data = JSON.stringify(response.data, null, 2); // null,2 sirve para renderizar el JSON, que lo muestre bonito, etc...
+                    $scope.stats = response.data;
+                });
+            }   
+    
+    
+    //GET A UN CONJUNTO CON PAGINACIÓN
         $scope.getData = function(){
            
             $http
@@ -27,8 +36,7 @@ angular
                     $scope.stats = response.data;
                 });
             
-        }   
-    //}
+        } 
    
         //MÉTODO PARA AÑADIR UN PAÍS    
         $scope.addStats = function(){
@@ -80,11 +88,9 @@ angular
                 .get($scope.url+"?apikey="+$scope.apikey+"&country="+$scope.newCountry.country+"&year="+$scope.newCountry.year)
                 .then(function(response){
                     console.log("The search of: "+$scope.newCountry.country +" in year "+ $scope.newCountry.year+ " works correctly");
-                   $scope.data = JSON.stringify(response.data, null, 2); // null,2 sirve para renderizar el JSON, que lo muestre bonito, etc...
+                    $scope.data = JSON.stringify(response.data, null, 2); // null,2 sirve para renderizar el JSON, que lo muestre bonito, etc...
                     $scope.stats = response.data; 
                 });
         }
-      
-     
-     //refresh();           
+           
 }]);  
