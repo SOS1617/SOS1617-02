98cb5769d175977bc968b0b4961d4768345f5cf9
SOS1617_agc <andresgarciacastilla@gmail.com>
2017-04-18 17:11:19 +0000

    Hecho el frontend de Andrés

---
+++ public/angularGDP/gdp-manager-app.js
@@ -0,0 +1,2 @@
+angular.module("GdpManagerApp",[]);
+            console.log("GdpManagerApp Initialized!!");
---
+++ public/angularGDP/list-ctrl.js
@@ -0,0 +1,94 @@
+angular
+    .module("GdpManagerApp")
+    .controller("list-ctrl",["$scope", "$http", function($scope, $http){
+        
+        $scope.url = "/api/v1/gdp-population-stats";
+        $scope.offset = 0;
+        $scope.limit = 0;
+        console.log("Controller initialized ");
+        
+        //Load initial data
+        $scope.loadInitialData= function(){
+            $http.get($scope.url+"/loadInitialData?apikey="+$scope.apikey)
+            .then(function(){
+                console.log("Load initial data: OK");
+            
+            })
+        }
+            
+        //Get every element
+        $scope.getAllData = function(){
+           
+            $http
+                .get($scope.url+"?apikey="+ $scope.apikey)
+                .then(function(response){
+                    $scope.data = JSON.stringify(response.data, null, 2); 
+                    $scope.countries = response.data;
+                    console.log("GetAllData OK");
+                });
+            
+        } 
+        
+        //Get some elements (pagination)
+        $scope.getDataPaginated = function(){
+           
+            $http
+                .get($scope.url+"?apikey="+ $scope.apikey +"&limit="+ $scope.limit +"&offset="+$scope.offset)
+                .then(function(response){
+                    $scope.data = JSON.stringify(response.data, null, 2); 
+                    $scope.countries = response.data;
+                    console.log("getDataPaginated OK");
+                });
+            
+        } 
+        
+        //Add an element
+        $scope.addData = function(){
+        $http
+            .post($scope.url+"?apikey="+ $scope.apikey, $scope.newCountry)
+            .then(function(response){
+                console.log($scope.newCountry.country + "Data added." );
+            });
+        } 
+        
+        //Edit an element
+        $scope.editData = function(){
+            $http
+                .put($scope.url +"/"+ $scope.newCountry.country + "?apikey="+ $scope.apikey, $scope.newCountry)
+                .then(function(response){
+                    console.log("editData OK");
+                });
+        }
+        
+        
+        //Delete every element
+        $scope.deleteAllData = function(){
+            $http
+                .delete($scope.url+"?apikey="+ $scope.apikey)
+                .then(function(response){
+                    console.log("deleteAllData OK");
+                });
+        }
+        
+        //Delete an element
+        $scope.deleteOneData = function(country,year){
+            $http
+                .delete($scope.url +"/"+ country +"/?apikey="+$scope.apikey)
+                .then(function(response){
+                    console.log("deleteOneData OK");
+                });
+        } 
+        
+        
+        //Search!!
+        $scope.search = function(){
+            $http
+                .get($scope.url+"?apikey="+$scope.apikey+"&country="+$scope.newCountry.country+"&year="+$scope.newCountry.year)
+                .then(function(response){
+                    console.log("search OK");
+                    $scope.data = JSON.stringify(response.data, null, 2);
+                    $scope.countries = response.data; 
+                });
+        }
+           
+}]);  
