cde546bc20792a8339eb4cc63895b21816b68436
SOS1617_aml <antony7_95@hotmail.com>
2017-05-31 13:14:19 +0000

    final day

--- public/angularRPC/google-graph-ctrl.js
+++ public/angularRPC/google-graph-ctrl.js
@@ -16,7 +16,7 @@ angular
         //RPCrpc
         $scope.rpcyear = [];
         $scope.rpcvariation = [];
-        $scope.rpcvariationdata = {};
+        var rpcvariationdata = [];
          
         console.log("Google graph initialized");
        function capitalizeFirstLetter(string) {
@@ -25,7 +25,7 @@ angular
 
 
             ////////////////////
-            /////DATOS Google/////
+            /////DATOS REST COUNTRIES API/////
             ////////////////////
                 
      $http.get("https://restcountries.eu/rest/v2/all").then(function(response){
@@ -60,142 +60,38 @@ angular
                     ////////////////////////////
                     ////COMPARATIVA RPC 2017////
                     ////////////////////////////
-                    Highcharts.chart('container',{
-                        title: {
-                            text: 'WORLD RPC integrated with SEVICI'
-                        },
-                        chart: {
-                            type: 'area'
-                        },
-                        xAxis: {
-                            categories: $scope.categorias
-                        },
-                        legend: {
-                            layout: 'vertical',
-                            floating: true,
-                            backgroundColor: '#FFFFFF',
-                            //align: 'left',
-                            verticalAlign: 'top',
-                            align: 'right',
-                            y: 20,
-                            x: 0
-                        },
-                        tooltip: {
-                            formatter: function () {
-                                return '<b>' + this.series.name + '</b><br/>' +
-                                   this.x + ': ' + this.y;
-                            }
-                        },
-                        series:[{
-                            name: 'Countries:',
-                            data: $scope.categorias,
-                        },
-                        {
-                            name: 'Area',
-                            data: $scope.areas,
-                        },
-                        {
-                            name: 'RPC Year Variation',
-                            data: $scope.rpcvariation
-                        }]
-                    });});
+                    });
          
      });
      
      
      
-     //console.log(rpcvariationdata);
-     
-     FusionCharts.ready(function () {
-    var salesChart = new FusionCharts({
-    type: 'MSColumn2D',
-    renderAt: 'chart-container',
-    width: '600',
-    height: '400',
-    dataFormat: 'json',
-    dataSource: {
-        "chart": {
-            "caption": "Sales report of Apple products",
-            "subcaption": "In Billion $",
-            "yaxismaxvalue": "250",
-            "decimals": "0",
-            "numberprefix": "$",
-            "numbersuffix": "B",
-            "placevaluesinside": "1",
-            "rotatevalues": "0",
-            "divlinealpha": "50",
-            "plotfillalpha": "80",
-            "drawCrossLine": "1",
-            "crossLineColor": "#cc3300",
-            "crossLineAlpha": "100",
-            "theme": "zune"
-        },
-        "categories": [{
-            "category": [{
-                    "label": "2012"
-                },
-                {
-                    "label": "2013"
-                },
-                {
-                    "label": "2014"
-                },
-                {
-                    "label": "2015"
-                },
-                {
-                    "label": "2016"
+    $http.get("/api/v1/rpc-stats"+ "?" + "apikey=" + $scope.apikey).then(function(response){
+            
+            //////////////////////////////
+            /////GOOGLE CHARTS////////////
+            //////////////////////////////
+            google.charts.load('current', {
+                'packages': ['geochart']
+            });
+            google.charts.setOnLoadCallback(drawRegionsMap);
+                function drawRegionsMap() {
+                    var myData = [['Country','Area','RPC- Variation']];
+                    var myData2=$scope.areas;
+                    
+                    
+                    response.data.forEach(function (d,i){
+                        myData.push([d.country,myData2[i],Number(d.rpcvariation)]);
+
+                    });
+                    var data = google.visualization.arrayToDataTable(myData);
+                    var options = {
+                        
+                    };
+                    var chart = new google.visualization.GeoChart(document.getElementById('map'));
+                    chart.draw(data, options);
                 }
-            ]
-        }],
-        "dataset": [{
-                "seriesname": "iPod",
-                "data": [$scope.rpcvariationdata
-                    ]
-            },
-            {
-                "seriesname": "iPhone",
-                "data": [{
-                        "value": "125.04"
-                    },
-                    {
-                        "value": "150.26"
-                    },
-                    {
-                        "value": "169.22"
-                    },
-                    {
-                        "value": "231.22"
-                    },
-                    {
-                        "value": "285.67"
-                    }
-                ]
-            },
-            {
-                "seriesname": "iPad",
-                "data": [{
-                        "value": "58.31"
-                    },
-                    {
-                        "value": "71.04"
-                    },
-                    {
-                        "value": "67.99"
-                    },
-                    {
-                        "value": "54.85"
-                    },
-                    {
-                        "value": "60.53"
-                    }
-                ]
-            }
-        ]
-    }
-})
-    .render();
-});
+            });
                
 
 }]);
